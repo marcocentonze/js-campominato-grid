0cdffcbc7961bb60d5f6233a95d22807fbcc8eaf
marcocentonze <marco.centonze27@gmail.com>
2023-09-01 16:52:16 +0200

    add final commit

--- assets/script.js
+++ assets/script.js
@@ -10,20 +10,32 @@ Ad esempio:
 -Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 -Le validazioni e i controlli possiamo farli anche in un secondo momento. */
 
+
+//evento per il bottone da cliccare
+document.getElementById('start_game').addEventListener('click', startGame);
+//imposto il limite di 100(meglio fuori dalle funzioni)
+const limit = 100;
+
+function startGame() {
+    //Selezione l'elemento field della DOM
+    const fieldElement = document.querySelector('.field');
+    //richiamo la funzione per dire dove si andrà a stampare(dentro field)
+    generateField(fieldElement, limit);
+}
+
 //definiamo la funzione per generare la griglia
 function generateField(domElement, limit) {
-  // Genero la griglia in pagina
-  for (let i = 0; i < limit; i++) {
-    //creo un elemento nel markup per rappresentare la singoal cella
-    const cellElement = document.createElement("div");
-    //aggiungo la classe "cell" e poi stampo in pagina
-    cellElement.className = "cell";
-    cellElement.append(i + 1);
-    domElement.append(cellElement);
-  }
+    // Genero la griglia in pagina
+    for (let i = 0; i < limit; i++) {
+        //creo un elemento nel markup per rappresentare la singoal cella
+        const cellElement = document.createElement("div");
+        //aggiungo la classe "cell" e poi stampo in pagina
+        cellElement.className = "cell";
+        cellElement.append(i + 1);
+        domElement.append(cellElement);
+        //aggiungo eventolistener quando clicco sulla cella per cambiare colore
+        cellElement.addEventListener('click', function () {
+            this.classList.toggle('bg-dark-blue'); // Alterna la classe 'bg-green' per cambiare il colore
+        });
+    }
 }
-//aggiungo eventolistener quando clicco sulla cella per cambiare colore
-
-//funzione per avviare il gioco
-
-//evento listener
